a7129244b4623e391fdd1f7f231a6e4c75e30ce9
msamoi <msamoi@taltech.ee>
2023-09-26 15:34:40 +0300

    Initial commit for HW02

---
+++ HW02/ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+// Project for the Diffie-Hellman algorithm implementation + prime number finder
