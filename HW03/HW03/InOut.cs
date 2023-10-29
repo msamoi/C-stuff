6b93d4849b6ae47ad6d5768e3e87ef1b12f7a8a3
Martenpilv <tublimarten@gmail.com>
2023-10-29 21:06:27 +0200

    removed unneeded function

--- HW03/HW03/InOut.cs
+++ HW03/HW03/InOut.cs
@@ -2,16 +2,6 @@
 
 public static class InOut
 {
-    public static ulong GetNumberFromUser()
-    {
-        do
-        {
-            var input = Console.ReadLine();
-            if (ulong.TryParse(input, out var outvar)) return outvar;
-            Console.WriteLine("Input is not a number!");
-        } while (true);
-    }
-
     public static string GetChoice()
     {
         var userChoice = Console.ReadLine()?.ToUpper().Trim();
